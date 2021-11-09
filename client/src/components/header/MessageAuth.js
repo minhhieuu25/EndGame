90ef5dc55d0f9ddd32cfd7146e921ebdf389d474
lehuuhlong <lehuuhlong@gmail.com>
2021-11-09 21:13:30 +0700

    long911

--- client/src/components/header/MessageAuth.js
+++ client/src/components/header/MessageAuth.js
@@ -9,25 +9,33 @@ const MessageAuth = () => {
 
     return (
         <>
-            {auth.token ?
-                <div className="message-auth">
-                    <div className="card">
-                        <div className="header-message" onClick={() => setTypeContent(!typeContent)}>
-                            <span>Message</span>
-                            <span className="right-header">
-                                <Link to="/message"><i className="fab fa-facebook-messenger"></i></Link>
-                                {typeContent ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
-                            </span>
-                        </div>
+            <div className="message-auth">
+                <div className="card">
+                    <div className="header-message" onClick={() => setTypeContent(!typeContent)}>
+                        <span>Message</span>
+                        <span className="right-header">
+                            <Link to="/message"><i className="fab fa-facebook-messenger"></i></Link>
+                            {typeContent ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
+                        </span>
+                    </div>
+                    {auth.token ?
                         <div className="content-message" style={{ display: `${typeContent ? 'block' : 'none'}` }}>
                             <span>Connect with others on RankWorks by beginning a new conversation.</span>
                             <Link to="/message">
                                 <button className="btn btn-new">New message</button>
                             </Link>
                         </div>
-                    </div>
-                </div> : null
-            }
+                        :
+                        <div className="content-message" style={{ display: `${typeContent ? 'block' : 'none'}` }}>
+                            <span>Connect with others on RankWorks by beginning a new conversation.</span>
+                            <Link to="/login">
+                                <button className="btn btn-new">Login</button>
+                            </Link>
+                        </div>
+                    }
+                </div>
+            </div>
+
         </>
     )
 }
